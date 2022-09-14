64962d6c142244e8997087b1fefb6bed4dcfe8b7
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-14 18:12:33 -0300

    silence an unused Interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,6 +4,6 @@ export interface IProduct {
   amount: string;
 }
 
-export interface Product extends IProduct {
-  id: number;
-}
+// export interface Product extends IProduct {
+//   id: number;
+// }

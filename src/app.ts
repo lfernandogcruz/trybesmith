00139edafd16f8120540cac07048a22f67011490
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-14 17:22:29 -0300

    feat: req 01 - applies Product route on aap file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/products', routes.productsRoute);
+
 export default app;
-/* inoffensive line for first commit */

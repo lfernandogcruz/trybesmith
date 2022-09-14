929d9dcd4a65e2b9892d2d2103a5119198ed9409
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-14 18:11:14 -0300

    feat: req 02 - creates Route for listing All products at DB

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -5,5 +5,6 @@ import productsController from '../controllers/productsController';
 const productsRoute = express.Router();
 
 productsRoute.post('/', productsController.create);
+productsRoute.get('/', productsController.findAll);
 
 export default productsRoute;

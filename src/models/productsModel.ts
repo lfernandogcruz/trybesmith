6b33db48c896420e1d2ea2dc7aa2157f434a3ce3
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:20:06 -0300

    style: clean up of unused comments lines

--- src/models/productsModel.ts
+++ src/models/productsModel.ts
@@ -2,7 +2,6 @@ import { ResultSetHeader } from 'mysql2';
 
 import connection from './connection';
 import { IProduct } from '../interfaces';
-// import { IProduct, Product } from '../interfaces';
 
 const create = async (product: IProduct): Promise<ResultSetHeader> => {
   const { name, amount } = product;
@@ -12,18 +11,12 @@ const create = async (product: IProduct): Promise<ResultSetHeader> => {
   const values = [name, amount];
 
   const [result] = await connection.execute<ResultSetHeader>(query, values);
-  // console.log('<><><><><><<>< RESULT - ', result);
-  // const { insertId: id } = result;
-
-  // const newProduct: Product = { ...product, id };
-  // return newProduct;
   return result;
 };
 
 const findAll = async (): Promise<ResultSetHeader> => {
   const query = 'SELECT * from Trybesmith.Products';
   const [result] = await connection.execute<ResultSetHeader>(query);
-  // console.log('<><><><><><<>< RESULT - ', result);
   return result;
 };
 

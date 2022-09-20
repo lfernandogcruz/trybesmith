8083ee7ec448b3a2ab4a3e971f3fc3f1c4ae3e73
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:22:40 -0300

    style: clean up of unused comments lines

--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -2,7 +2,6 @@ import { ResultSetHeader } from 'mysql2';
 
 import connection from './connection';
 import { IUser } from '../interfaces';
-// import { IUser, user } from '../interfaces';
 
 const create = async (user: IUser): Promise<ResultSetHeader> => {
   const { username, classe, level, password } = user;
@@ -12,11 +11,7 @@ const create = async (user: IUser): Promise<ResultSetHeader> => {
   const values = [username, classe, level, password];
 
   const [result] = await connection.execute<ResultSetHeader>(query, values);
-  // console.log('<><><><><><<>< RESULT MODEL - ', result);
-  // const { insertId: id } = result;
-
-  // const newuser: user = { ...user, id };
-  // return newuser;
+ 
   return result;
 };
 

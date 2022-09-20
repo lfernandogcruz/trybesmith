c1526cda9a26921598e18ef34d14cfe8c7ae099b
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:25:41 -0300

    feat: req 04 - creates Order Interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -11,6 +11,9 @@ export interface IUser {
   level: number;
   password: string;
 }
-// export interface Product extends IProduct {
-//   id: number;
-// }
+
+export interface IOrder {
+  id: number;
+  userId: number;
+  productsIds: number[];
+}

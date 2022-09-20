3e1506a528ff5c8d234930bfb45eed58ed711a37
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:17:16 -0300

    refact: changes POST Products body validation from services layer to middleware

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import productsController from '../controllers/productsController';
+import newProductBodyValidation from '../middlewares/newProductBodyValidation';
 
 const productsRoute = express.Router();
 
-productsRoute.post('/', productsController.create);
+productsRoute.post('/', newProductBodyValidation, productsController.create);
 productsRoute.get('/', productsController.findAll);
 
 export default productsRoute;

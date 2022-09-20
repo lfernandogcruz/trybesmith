14bf83ce5343c9fb78ab9ee711ea574e227c4d05
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 01:35:50 -0300

    feat: req 07 - updates middleware validation for POST on Users functions

--- src/middlewares/newUserBodyValidation.ts
+++ src/middlewares/newUserBodyValidation.ts
@@ -1,16 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
-import HttpException from '../helpers/http.exception';
 import { IUser } from '../interfaces';
 
-const INVALID_DATA_ERR = 'Dados inválidos!';
-
-const newUserBodyValidation = (req: Request, _res: Response, next: NextFunction) => {
+export const newUserLevelValidation = (req: Request, res: Response, next: NextFunction) => {
   const user = req.body as IUser;
-  if (!user.level) throw new HttpException(400, INVALID_DATA_ERR);
-  if (!user.password) throw new HttpException(400, INVALID_DATA_ERR);
-  if (!user.username) throw new HttpException(400, INVALID_DATA_ERR);
-  if (!user.classe) throw new HttpException(400, INVALID_DATA_ERR);
+  if (user.level === undefined || user.level === null) {
+    return res.status(400).json({ message: '"level" is required' });
+  }
+  if (typeof user.level !== 'number') {
+    return res.status(422).json({ message: '"level" must be a number' });
+  }
+  if (user.level < 1) {
+    return res.status(422).json({ message: '"level" must be greater than or equal to 1' });
+  }
   next();
 };
 
-export default newUserBodyValidation;
+export const newUserPasswordValidation = (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as IUser;
+  if (!user.password) return res.status(400).json({ message: '"password" is required' });
+  if (typeof user.password !== 'string') {
+    return res.status(422).json({ message: '"password" must be a string' });
+  }
+  if (user.password.length < 8) {
+    return res.status(422).json({
+      message: '"password" length must be at least 8 characters long' });
+  }
+  next();
+};
+
+export const newUserNameValidation = (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as IUser;
+  if (!user.username) return res.status(400).json({ message: '"username" is required' });
+  if (typeof user.username !== 'string') {
+    return res.status(422).json({ message: '"username" must be a string' });
+  }
+  if (user.username.length < 3) {
+    return res.status(422).json({
+      message: '"username" length must be at least 3 characters long' });
+  }
+  next();
+};
+
+export const newUserClassValidation = (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as IUser;
+  if (!user.classe) return res.status(400).json({ message: '"classe" is required' });
+  if (typeof user.classe !== 'string') {
+    return res.status(422).json({ message: '"classe" must be a string' });
+  }
+  if (user.classe.length < 3) {
+    return res.status(422).json({ message: '"classe" length must be at least 3 characters long' });
+  }
+  next();
+};

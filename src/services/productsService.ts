28899541aef1c19305397318f028154ec98098e1
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:17:48 -0300

    refact: changes POST Products body validation from services layer to middleware

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,17 +1,7 @@
 import { IProduct } from '../interfaces';
 import productsModel from '../models/productsModel';
-import HttpException from '../helpers/http.exception';
-
-const isValid = (product: IProduct) => {
-  if (!product.name || typeof product.name !== 'string') return false;
-  if (!product.amount || typeof product.amount !== 'string') return false;
-  return true;
-};
 
 const create = async (product: IProduct) => {
-  if (!isValid(product)) {
-    throw new HttpException(400, 'Dados inválidos!');
-  }
   const { insertId } = await productsModel.create(product);
   const newProduct = { ...product, id: insertId };
   return newProduct;

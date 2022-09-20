c949d13c69ab809bb963e24b1f6ccd43f3ef4d2d
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-19 21:01:23 -0300

    feat: req 03 - creates a Middleware validation for the body on POST at Users

---
+++ src/middlewares/newUserBodyValidation.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../helpers/http.exception';
+import { IUser } from '../interfaces';
+
+const INVALID_DATA_ERR = 'Dados inválidos!';
+
+const newUserBodyValidation = (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body as IUser;
+  if (!user.level) throw new HttpException(400, INVALID_DATA_ERR);
+  if (!user.password) throw new HttpException(400, INVALID_DATA_ERR);
+  if (!user.username) throw new HttpException(400, INVALID_DATA_ERR);
+  if (!user.classe) throw new HttpException(400, INVALID_DATA_ERR);
+  next();
+};
+
+export default newUserBodyValidation;

205b09875d445e74a8c534c0d8e497d893da9e93
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 01:12:07 -0300

    feat: req 06 - updates body validation middlewares for POST Product

--- src/middlewares/newProductBodyValidation.ts
+++ src/middlewares/newProductBodyValidation.ts
@@ -1,14 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import HttpException from '../helpers/http.exception';
 import { IProduct } from '../interfaces';
 
-const INVALID_DATA_ERR = 'Dados inválidos!';
-
-const newProductBodyValidation = (req: Request, _res: Response, next: NextFunction) => {
+export const newProductNameValidation = (req: Request, res: Response, next: NextFunction) => {
   const product = req.body as IProduct;
-  if (!product.name) throw new HttpException(400, INVALID_DATA_ERR);
-  if (!product.amount) throw new HttpException(400, INVALID_DATA_ERR);
+  if (!product.name) return res.status(400).json({ message: '"name" is required' });
+  if (typeof product.name !== 'string') {
+    return res.status(422).json({ message: '"name" must be a string' });
+  }
+  if (product.name.length < 3) {
+    return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
+  }
   next();
 };
 
-export default newProductBodyValidation;
+export const newProductAmountValidation = (req: Request, res: Response, next: NextFunction) => {
+  const product = req.body as IProduct;
+  if (!product.amount) return res.status(400).json({ message: '"amount" is required' });
+  if (typeof product.amount !== 'string') {
+    return res.status(422).json({ message: '"amount" must be a string' });
+  }
+  if (product.amount.length < 3) {
+    return res.status(422).json({ message: '"amount" length must be at least 3 characters long' });
+  }
+  next();
+};

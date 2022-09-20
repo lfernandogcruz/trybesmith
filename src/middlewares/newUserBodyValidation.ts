3acb0e30094ca15f8bc3b779da355fcbc05b652c
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-20 00:20:56 -0300

    style: clean up of unused comments lines

--- src/middlewares/newUserBodyValidation.ts
+++ src/middlewares/newUserBodyValidation.ts
@@ -4,7 +4,7 @@ import { IUser } from '../interfaces';
 
 const INVALID_DATA_ERR = 'Dados inválidos!';
 
-const newUserBodyValidation = (req: Request, res: Response, next: NextFunction) => {
+const newUserBodyValidation = (req: Request, _res: Response, next: NextFunction) => {
   const user = req.body as IUser;
   if (!user.level) throw new HttpException(400, INVALID_DATA_ERR);
   if (!user.password) throw new HttpException(400, INVALID_DATA_ERR);

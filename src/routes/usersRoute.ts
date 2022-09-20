48983b4727bedf008332dae33faaf03f3905ece4
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-09-19 21:19:04 -0300

    feat: req 03 - creates an User Router

---
+++ src/routes/usersRoute.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import usersController from '../controllers/usersController';
+import newUserBodyValidation from '../middlewares/newUserBodyValidation';
+import tokenGenerator from '../middlewares/tokenGenerator';
+
+const usersRoute = express.Router();
+
+usersRoute.post('/', newUserBodyValidation, tokenGenerator, usersController.create);
+// usersRoute.get('/', usersController.findAll);
+
+export default usersRoute;
